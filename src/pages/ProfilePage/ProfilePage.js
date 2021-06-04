ae39c9c453b9b689792557d0484763957b408af5
ikaamalin <85057635+ikaamalin@users.noreply.github.com>
2021-06-04 20:33:38 +0800

    Update ProfilePage.js
    syafiqah 4/6/2021 7.10

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,127 +1,103 @@
-import React, { Component } from 'react';
-import { TextInput, TouchableOpacity, View, Image, ActivityIndicator } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import React, { useState } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { firebase } from '../../firebase/config'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
 
-class ProfilePage extends Component {
+export default function ProfilePage(props) {
+const Name = props.extraData.fullName
+const Email = props.extraData.email
+const MatricNum = props.extraData.matricNum
+const Kulliyyah = props.extraData.kulliyyah
+const contactNum = props.extraData.contactNum
+const address = props.extraData.address
 
-    constructor() {
-        super();
-        this.state = {
-            fullName: '',
-            matricNum: '',
-            kulliyyah: '',
-            contactNum: '',
-            address: '',
-        };
-    }
 
-    componentDidMount() {
-        const dbRef = firebase.firestore()
-            .collection('users')
-            .where("authorID", "==", firebase.auth().currentUser.uid)
-        dbRef.get().then((res) => {
-          if (res.exists) {
-            const user = res.data();
-            this.setState({
+    return (
+      <View style={styles.container}>
+          <KeyboardAwareScrollView
+              style={{ flex: 1, width: '100%' }}
+              keyboardShouldPersistTaps="always">
+              <Image
+                  style={styles.logo}
+                  source={require('../../../assets/logoiium.png')}
+              />
               
-              fullName: user.fullName,
-              matricNum: user.matricNum,
-              kulliyyah: user.kulliyyah,
-              contactNum: user.contactNum,
-              address: user.address,
-              
-            });
-          } else {
-            console.log("Error: No such data available.");
-          }
-        });
-      }
-    
-      inputValueUpdate = (val, prop) => {
-        const state = this.state;
-        state[prop] = val;
-        this.setState(state);
-      }
-    
-      updateUser() {
-        const updateDBRef = firebase.firestore()
-          .collection('users')
-          .where("authorID", "==", firebase.auth().currentUser.uid)
-        updateDBRef.set({
-          fullName: this.state.fullName,
-          matricNum: this.state.matricNum,
-          kulliyyah: this.state.kulliyyah,
-          contactNum: this.state.contactNum,
-          address: this.state.address,
-        }).then((docRef) => {
-          this.setState({
-            fullName: '',
-            matricNum: '',
-            kulliyyah: '',
-            contactNum: '',
-            address: '',
-            
-          });
-          console.log("Information Updated!");
-        })
-        .catch((error) => {
-          console.error("Error: ", error);
-        });
-      }
+              <View>
+    <Text>Name: </Text>
+    <TextInput
+      style={{
+        height: 40,
+        borderColor: 'white',
+        borderWidth: 1,
+        backgroundColor:'white' 
+      }}
+      defaultValue= {Name}
+    />
+  </View>
+               <View>
+    <Text>Email: </Text>
+    <TextInput
+      style={{
+        height: 40,
+        borderColor: 'white',
+        borderWidth: 1,
+        backgroundColor:'white' 
+      }}
+      defaultValue= {Email}
+    />
+  </View>
+  <View>
+    <Text>Matric Number: </Text>
+    <TextInput
+      style={{
+        height: 40,
+        borderColor: 'white',
+        borderWidth: 1,
+        backgroundColor:'white' 
+      }}
+      defaultValue= {MatricNum}
+    />
+  </View>
+  <View>
+    <Text>Kulliyyah: </Text>
+    <TextInput
+      style={{
+        height: 40,
+        borderColor: 'white',
+        borderWidth: 1,
+        backgroundColor:'white' 
+      }}
+      defaultValue= {Kulliyyah}
+    />
+  </View>
+  <View>
+    <Text>Contact Number: </Text>
+    <TextInput
+      style={{
+        height: 40,
+        borderColor: 'white',
+        borderWidth: 1,
+        backgroundColor:'white' 
+      }}
+      defaultValue= {contactNum}
+    />
+  </View>
+  <View>
+    <Text>Address: </Text>
+    <TextInput
+      style={{
+        height: 40,
+        borderColor: 'white',
+        borderWidth: 1,
+        backgroundColor:'white' 
+      }}
+      defaultValue= {address}
+    />
+  </View>
 
-      render() {
-        return (
-        <View>
-          <KeyboardAwareScrollView 
-            style={{ flex: 1, width: '100%' }}
-            keyboardShouldPersistTaps="always">
-                <Image style = { styles.logo }
-                    source={require('../../../assets/logoiium.png')}
-                />
-
-               <TextInput
-                  style={styles.input}
-                  placeholder={'Full Name'}
-                  value={this.state.fullName}
-                  onChangeText={(val) => this.inputValueUpdate(val, 'fullName')}
-               />
-
-                <TextInput
-                  style={styles.input}
-                  placeholder={'Matric Number'}
-                  value={this.state.matricNum}
-                  onChangeText={(val) => this.inputValueUpdate(val, 'matricNum')}
-               />
-
-                <TextInput
-                  style={styles.input}
-                  placeholder={'Kulliyyah'}
-                  value={this.state.kulliyyah}
-                  onChangeText={(val) => this.inputValueUpdate(val, 'kulliyyah')}
-               />
-
-                <TextInput
-                  style={styles.input}
-                  multiline={true}
-                  numberOfLines={2}
-                  placeholder={'Address'}
-                  value={this.state.address}
-                  onChangeText={(val) => this.inputValueUpdate(val, 'address')}
-               />
-
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => this.updateUser()}>
-                    <Text style={styles.buttonTitle}>Update Account</Text>
-                </TouchableOpacity>
-
+             
           </KeyboardAwareScrollView>
-        </View>
-        );
-      }
-
+      </View>
+  )
 }
-
-export default ProfilePage;
